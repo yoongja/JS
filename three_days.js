799d45cbfdfef6b3d349db561dbe506654b517a6
yoongja <83260809+yoongja@users.noreply.github.com>
2021-06-30 17:49:43 +0900

    Create three_days.js
    js2주 챌린지 3일차수업

---
+++ three_days.js
@@ -0,0 +1,22 @@
+const calculator = {
+    add:function(a,b) {
+        console.log(a+b);
+    },
+    subtract:function(a,b){
+        console.log(a-b);
+    },
+    divide:function(a,b){
+        console.log(a/b);
+    },
+    mul:function(a,b){
+        console.log(a*b);
+    },
+    multiplication:function(a,b){
+        console.log(a**b);
+    }
+};
+calculator.add(3,2);
+calculator.subtract(3,2);
+calculator.divide(3,2);
+calculator.mul(3,2);
+calculator.multiplication(3,2);
